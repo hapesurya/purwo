684d16f82c2e9496fc6d256ec721bb269d5b6fbc
hapesurya <hendra.permana@gmail.com>
2023-11-22 15:49:44 +0700

    revisi app dan template

--- app.py
+++ app.py
@@ -84,16 +84,60 @@ def capture():
         base64 = (image_data_url[-55:-5])
 
         now = datetime.datetime.now()
-        jam = now.strftime("%Y-%m-%d %H-%M-%S")
+        jam = now.strftime("%Y-%m-%d %H:%M:%S")
 
         # MQTT Client Setup
-        # mqtt_client = mqtt.Client()
+        mqtt_client = mqtt.Client("P1")
+
+        FACE_COMMAND_TOPIC = "mqtt/face/basic"
         
-        # # Connect MQTT
-        # mqtt_client.connect(host='broker.mqtt-dashboard.com', port=1883)
-        # # Send timestamp as a message to the MQTT broker with the topic '/service'
-        # mqtt_client.publish(topic=str(service_key), payload=str(base64), qos=0)
-        # # Disconnect from MQTT broker
+        msg_id    = round(time.time()*1000)
+        tiket_id  = "QR%s" % msg_id       # untuk personId
+        image     = base64
+        tanggal   = now.strftime("%Y-%m-%d %H:%M:%S")
+        tgl       = now.strftime("%Y-%m-%d")
+        hangus       = "23:59:59"
+
+        msg = {"msg":
+        {
+            "messageId":"{}".format(msg_id),
+            "operator":"EditPerson",
+            "info":
+            {
+            "personId":"{}".format(tiket_id),
+            "customId":"{}".format(msg_id),
+            "name": "{}{}".format(ks, antrian),
+            "nation":1,
+            "gender":0,
+            "birthday":"{}".format(tanggal),
+            "address":"Purworejo",
+            "idCard":"421381199504030001",
+            "tempCardType":0,
+            "EffectNumber":3,
+            "cardValidBegin":"{}".format(tanggal),
+            "cardValidEnd":"{} {}".format(tgl, hangus),
+            "telnum1":"18888888888",
+            "Native": "Polres Purworejo, Jawa Tengah",
+            "cardType2":0,
+            "cardNum2":"",
+            "notes":"{}",
+            "personType":0,
+            "cardType":0,
+            "dwidentity":0,
+            "pic": "{}".format(image)
+            }
+        }
+        }
+        #(msg_id,tiket_id,msg_id,tanggal,antrian,image) #.format(mid=msg_id, tid=tiket_id, tno=antrian, tgl=tanggal, img=image)
+        msg = json.dumps(msg, indent = 4)
+        print(msg)
+        print(FACE_COMMAND_TOPIC)
+
+        # Connect MQTT
+        mqtt_client.connect(host='localhost', port=1883)
+        # Send timestamp as a message to the MQTT broker with the topic 'SN FR nya'
+        mqtt_client.publish(topic=FACE_COMMAND_TOPIC, payload=msg, qos=0)
+        # Disconnect from MQTT broker
         # mqtt_client.disconnect()
 
         # Generate a QR code based on the image_data_url now
@@ -118,42 +162,42 @@ def capture():
 
         # Setting Printer
         # Create a new instance of the printer object
-        printer = Usb(0x4b43, 0x3830, 0, 0x81, 0x03)
+        # printer = Usb(0x4b43, 0x3830, 0, 0x81, 0x03)
         
-        # Mulai Print Text Tiketnya
-        # Set align
-        printer.set(align='center')
-        printer.text('SATPAS POLRES PURWOREJO\n')
-        printer.text('\n')
-        printer.set(align='center')
-        printer.text('------------------------\n')
-        printer.set(align='center')
-        printer.text('\n')
-        printer.text(lay)
-        printer.text('\n')
-        printer.set(align='center')
-        printer.text((ks) + str(antrian))
-        printer.text('\n')
-        printer.set(align='center')
-        printer.text('------------------------\n')
-        printer.set(align='center')
-        printer.text('\n')
-        #kode QR Code nya
-        printer.qr(base64, size=16)
-        printer.set(align='center')
-        printer.text('\n')
-        printer.text('------------------------\n')
-        printer.text('\n')
-        printer.set(align='center')
-        printer.text('Mohon sabar menunggu\n')
-        printer.set(align='center')
-        printer.text('\n')
-        printer.text(jam)
-        printer.text('\n')
-        printer.set(align='center')
+        # # Mulai Print Text Tiketnya
+        # # Set align
+        # printer.set(align='center')
+        # printer.text('SATPAS POLRES PURWOREJO\n')
+        # printer.text('\n')
+        # printer.set(align='center')
+        # printer.text('------------------------\n')
+        # printer.set(align='center')
+        # printer.text('\n')
+        # printer.text(lay)
+        # printer.text('\n')
+        # printer.set(align='center')
+        # printer.text((ks) + str(antrian))
+        # printer.text('\n')
+        # printer.set(align='center')
+        # printer.text('------------------------\n')
+        # printer.set(align='center')
+        # printer.text('\n')
+        # #kode QR Code nya
+        # printer.qr(base64, size=16)
+        # printer.set(align='center')
+        # printer.text('\n')
+        # printer.text('------------------------\n')
+        # printer.text('\n')
+        # printer.set(align='center')
+        # printer.text('Mohon sabar menunggu\n')
+        # printer.set(align='center')
+        # printer.text('\n')
+        # printer.text(jam)
+        # printer.text('\n')
+        # printer.set(align='center')
 
-        # Cut the paper
-        printer.cut()
+        # # Cut the paper
+        # printer.cut()
     return render_template('mqttgate/capture.html', qr_code_path=qr_code_path, ks=ks, lay=lay, queue_number=session['queue_number_' + service_key], jam=jam)
 
 ##===========================================================
@@ -164,62 +208,62 @@ def capture():
 
 ## the topic:
 ## mqtt/face/<ID-FACE_RCOGINZER>
-FACE_COMMAND_TOPIC = "mqtt/face/1962829"
+# FACE_COMMAND_TOPIC = "mqtt/face/1962829"
 
-def publish(data, pick):
+# def publish(data, pick):
 
-    msg_id    = round(time.time()*1000)
-    tiket_id  = "QR%s" % msg_id       # untuk personId
-    image     = data
-    tanggal   = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    tgl       = datetime.now().strftime("%Y-%m-%d")
-    jam       = "23:59:59"
+#     msg_id    = round(time.time()*1000)
+#     tiket_id  = "QR%s" % msg_id       # untuk personId
+#     image     = data
+#     tanggal   = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#     tgl       = datetime.now().strftime("%Y-%m-%d")
+#     jam       = "23:59:59"
 
-    msg = {"msg":
-      {
-        "messageId":"{}".format(msg_id),
-        "operator":"EditPerson",
-        "info":
-        {
-          "personId":"{}".format(tiket_id),
-          "customId":"{}".format(msg_id),
-          "name": "{}",
-          "nation":1,
-          "gender":0,
-          "birthday":"{}".format(tanggal),
-          "address":"Purworejo",
-          "idCard":"421381199504030001",
-          "tempCardType":0,
-          "EffectNumber":3,
-          "cardValidBegin":"{}".format(tanggal),
-          "cardValidEnd":"{} {}".format(tgl, jam),
-          "telnum1":"18888888888",
-          "Native": "Polres Purworejo, Jawa Tengah",
-          "cardType2":0,
-          "cardNum2":"",
-          "notes":"{}",
-          "personType":0,
-          "cardType":0,
-          "dwidentity":0,
-          "pic": "{}".format(image)
-        }
-      }
-    }
-    #(msg_id,tiket_id,msg_id,tanggal,antrian,image) #.format(mid=msg_id, tid=tiket_id, tno=antrian, tgl=tanggal, img=image)
-    msg = json.dumps(msg, indent = 4)
-    print(msg)
+#     msg = {"msg":
+#       {
+#         "messageId":"{}".format(msg_id),
+#         "operator":"EditPerson",
+#         "info":
+#         {
+#           "personId":"{}".format(tiket_id),
+#           "customId":"{}".format(msg_id),
+#           "name": "{}",
+#           "nation":1,
+#           "gender":0,
+#           "birthday":"{}".format(tanggal),
+#           "address":"Purworejo",
+#           "idCard":"421381199504030001",
+#           "tempCardType":0,
+#           "EffectNumber":3,
+#           "cardValidBegin":"{}".format(tanggal),
+#           "cardValidEnd":"{} {}".format(tgl, jam),
+#           "telnum1":"18888888888",
+#           "Native": "Polres Purworejo, Jawa Tengah",
+#           "cardType2":0,
+#           "cardNum2":"",
+#           "notes":"{}",
+#           "personType":0,
+#           "cardType":0,
+#           "dwidentity":0,
+#           "pic": "{}".format(image)
+#         }
+#       }
+#     }
+#     #(msg_id,tiket_id,msg_id,tanggal,antrian,image) #.format(mid=msg_id, tid=tiket_id, tno=antrian, tgl=tanggal, img=image)
+#     msg = json.dumps(msg, indent = 4)
+#     print(msg)
 
-    #msg = msg.format(tid=tiket_id, msg_id=msg_id, tno=antrian, tgl=tanggal, img=image)
+#     #msg = msg.format(tid=tiket_id, msg_id=msg_id, tno=antrian, tgl=tanggal, img=image)
     
-    ## instance
-    client = mqtt.Client("P1") #create new instance 
-    #connect to broker
-    broker_address = "localhost"
-    broker_port = 1883
-    client.connect(broker_address, port=broker_port)
+#     ## instance
+#     client = mqtt.Client("P1") #create new instance 
+#     #connect to broker
+#     broker_address = "localhost"
+#     broker_port = 1883
+#     client.connect(broker_address, port=broker_port)
     
-    ## send to face-recog 
-    client.publish(FACE_COMMAND_TOPIC, msg)
+#     ## send to face-recog 
+#     client.publish(FACE_COMMAND_TOPIC, msg)
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5000,  debug=True)
