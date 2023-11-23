2cb95883a17cc4b5235b11009cc6802c8199dc05
hapesurya <hendra.permana@gmail.com>
2023-11-23 13:23:49 +0700

    tambah fungsi database

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-## Yang belum dibuat:
+## Yang belum dilakukan:
 1. Database
 
-## Yang sudah dibuat:
+## Yang sudah dilakukan:
 1. 4 Halaman dengan foto menggunakan ijin camera browser
-2. MQTT sudah di test dari Flask ke MQTT Subscriber dengan topik sesuai dengan layanan yang sudah dipilih menggunakan pesan dari data wajah base64
-3. Setup printer thermal dengan driver escpos sudah disetting, tinggal di testing.
+2. MQTT sudah di test dari Flask ke Broker online dengan dengan topik sesuai dengan layanan yang sudah dipilih menggunakan pesan dari data wajah base64.
+3. Setup printer thermal sudah oke
 4. Nomor urut menggunakan fungsi session akan reset setiap hari
+5. Kirim data MQTT ke FR sudah oke
 
 ### Modul yang harus diinstal sudah ditulis di file requirement.# purwo
--- __init__.py
+++ __init__.py
@@ -6,9 +6,21 @@ import qrcode
 import os
 from escpos.printer import Usb
 import json
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class VisitorDB(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tanggal = db.Column(db.String(100), nullable=False)
+    layanan = db.Column(db.String(100), nullable=False)
+    antrian = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return '<Visitor %r>' % self.id
 
 #1. Reset queue number and date when the day changes
 #2. Get ks from URL query parameter
@@ -81,10 +93,11 @@ def capture():
         # Get image data from form
         image_data_url = request.form['img']
         # Data base64 diperkecil dulu
-        base64 = (image_data_url[-55:-5])
+        base64_edit = (image_data_url[-55:-5])
 
         now = datetime.datetime.now()
-        jam = now.strftime("%Y-%m-%d %H:%M:%S")
+        tanggaljam_now = now.strftime("%d-%m-%Y %H:%M:%S")
+        datehour_now = now.strftime("%Y-%m-%d %H:%M:%S")
 
         # MQTT Client Setup
         mqtt_client = mqtt.Client("P1")
@@ -93,9 +106,7 @@ def capture():
         
         msg_id    = round(time.time()*1000)
         tiket_id  = "QR%s" % msg_id       # untuk personId
-        image     = base64
-        tanggal   = now.strftime("%Y-%m-%d %H:%M:%S")
-        tgl       = now.strftime("%Y-%m-%d")
+        date       = now.strftime("%Y-%m-%d")
         hangus    = "23:59:59"
 
         msg = {"msg":
@@ -109,13 +120,13 @@ def capture():
             "name": "{}{}".format(ks, antrian),
             "nation":1,
             "gender":0,
-            "birthday":"{}".format(tanggal),
+            "birthday":"{}".format(date),
             "address":"Purworejo",
             "idCard":"421381199504030001",
             "tempCardType":0,
             "EffectNumber":3,
-            "cardValidBegin":"{}".format(tanggal),
-            "cardValidEnd":"{} {}".format(tgl, hangus),
+            "cardValidBegin":"{}".format(datehour_now),
+            "cardValidEnd":"{} {}".format(date, hangus),
             "telnum1":"18888888888",
             "Native": "Polres Purworejo, Jawa Tengah",
             "cardType2":0,
@@ -124,83 +135,100 @@ def capture():
             "personType":0,
             "cardType":0,
             "dwidentity":0,
-            "pic": "{}".format(image)
+            "pic": "{}".format(base64_edit)
             }
         }
         }
         #(msg_id,tiket_id,msg_id,tanggal,antrian,image) #.format(mid=msg_id, tid=tiket_id, tno=antrian, tgl=tanggal, img=image)
         msg = json.dumps(msg, indent = 4)
-        print(msg)
-        print(FACE_COMMAND_TOPIC)
+        # print(msg) #uncomment for testing
+        # print(FACE_COMMAND_TOPIC) #uncomment for testing
+
+        # Send QRCodeMQTT
+        msg_qrcode = {
+            "messageId":"{}".format(msg_id),
+            "operator": "ShowQRCode", 
+            "info": { 
+                "ImageType":"3",
+                "AbsX":"10",
+                "AbsY":"10",
+                "ImageW":"200",
+                "ImageH":"200",
+                "QRCodeData":"{}".format(base64_edit)
+                }
+            }
+        
+        msg_qrcode = json.dumps(msg_qrcode, indent = 4)
 
         # Connect MQTT
+        mqtt_client.username_pw_set(username='admin', password='admin')
         mqtt_client.connect(host='localhost', port=1883)
         # Send timestamp as a message to the MQTT broker with the topic 'SN FR nya'
         mqtt_client.publish(topic='mqtt/face/1962821', payload=msg, qos=0)
-        # mqtt_client.publish(topic='mqtt/face/1962821/Ack', payload=msg, qos=0)
+        mqtt_client.publish(topic='mqtt/face/1962821/QRcode', payload=msg_qrcode, qos=0)
 
         # Disconnect from MQTT broker
         # mqtt_client.disconnect()
 
         # Generate a QR code based on the image_data_url now
         qr = qrcode.QRCode(
-            version=1,
+            version=2,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
             box_size=10,
             border=4,
         )
-        qr.add_data(base64)
+        qr.add_data(base64_edit)
         qr.make(fit=True)
 
         # EXPERIMENT JIKA QRCODE MAU DITAMPILKAN DI LAYAR
         # Create a QR code image
-        qr_image_path = f'./static/photos/{jam}_qr_code.png'
+        qr_image_path = f'./static/photos/{datehour_now}_qr_code.png'
         qr_image = qr.make_image(fill_color="black", back_color="white")
         qr_image.save(qr_image_path)
 
         # # EXPERIMENT JIKA QRCODE MAU DITAMPILKAN DI LAYAR
         # Parsing gambar QRCode ke URL Queue
-        qr_code_path = f'./photos/{jam}_qr_code.png'
+        qr_code_path = f'./photos/{datehour_now}_qr_code.png'
 
         # Setting Printer
         # Create a new instance of the printer object
-        # printer = Usb(0x4b43, 0x3830, 0, 0x81, 0x03)
+        printer = Usb(0x4b43, 0x3830, 0, 0x81, 0x03)
         
-        # # Mulai Print Text Tiketnya
-        # # Set align
-        # printer.set(align='center')
-        # printer.text('SATPAS POLRES PURWOREJO\n')
-        # printer.text('\n')
-        # printer.set(align='center')
-        # printer.text('------------------------\n')
-        # printer.set(align='center')
-        # printer.text('\n')
-        # printer.text(lay)
-        # printer.text('\n')
-        # printer.set(align='center')
-        # printer.text((ks) + str(antrian))
-        # printer.text('\n')
-        # printer.set(align='center')
-        # printer.text('------------------------\n')
-        # printer.set(align='center')
-        # printer.text('\n')
-        # #kode QR Code nya
-        # printer.qr(base64, size=16)
-        # printer.set(align='center')
-        # printer.text('\n')
-        # printer.text('------------------------\n')
-        # printer.text('\n')
-        # printer.set(align='center')
-        # printer.text('Mohon sabar menunggu\n')
-        # printer.set(align='center')
-        # printer.text('\n')
-        # printer.text(jam)
-        # printer.text('\n')
-        # printer.set(align='center')
+        # Mulai Print Text Tiketnya
+        # Set align
+        printer.set(align='center')
+        printer.text('SATPAS POLRES PURWOREJO\n')
+        printer.text('\n')
+        printer.set(align='center')
+        printer.text('------------------------\n')
+        printer.set(align='center')
+        printer.text('\n')
+        printer.text(lay)
+        printer.text('\n')
+        printer.set(align='center')
+        printer.text((ks) + str(antrian))
+        printer.text('\n')
+        printer.set(align='center')
+        printer.text('------------------------\n')
+        printer.set(align='center')
+        printer.text('\n')
+        #kode QR Code nya
+        printer.qr(base64_edit, size=12)
+        printer.set(align='center')
+        printer.text('\n')
+        printer.text('------------------------\n')
+        printer.text('\n')
+        printer.set(align='center')
+        printer.text('Mohon sabar menunggu\n')
+        printer.set(align='center')
+        printer.text('\n')
+        printer.text(tanggaljam_now)
+        printer.text('\n')
+        printer.set(align='center')
 
-        # # Cut the paper
-        # printer.cut()
-    return render_template('mqttgate/capture.html', qr_code_path=qr_code_path, ks=ks, lay=lay, queue_number=session['queue_number_' + service_key], jam=jam)
+        # Cut the paper
+        printer.cut()
+    return render_template('mqttgate/capture.html', qr_code_path=qr_code_path, ks=ks, lay=lay, queue_number=session['queue_number_' + service_key], jam=tanggaljam_now)
 
 ##===========================================================
 # sending data (image (base64 jpeg format) to broker
